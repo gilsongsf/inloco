38d2620b8dd7f58b3e58c3fed19c4f751140fa5a
gilsongsf <31316960+gilsongsf@users.noreply.github.com>
2018-11-28 19:04:35 -0300

    Update env.py

--- env.py
+++ env.py
@@ -1,11 +1,5 @@
 #!/usr/bin/python
 import sys
 import yaml
-#env=sys.argv[1]
-f=open('envs.yml')
-envs=yaml.load(f)
 
-print(envs)
-print(f)
-#hosts=envs['envs'][env]['hosts']
-#for host in hosts: print host
+print("executando script")
